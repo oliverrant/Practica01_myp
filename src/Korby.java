978468df480fbbe7d3d8ffdc6a83704d7dbc9ebb
Christian Gutiérrez <91112471+ChristianGtz@users.noreply.github.com>
2026-03-01 16:38:53 -0600

    Update Korby.java

--- src/Korby.java
+++ src/Korby.java
@@ -1,5 +1,12 @@
+/**
+ * Representa al personaje Korby, famoso por replicar las habilidades
+ * de los enemigos que consume.
+ */
 public class Korby extends Personaje {
-     
+
+    /**
+     * Construye a Korby con 100 puntos de vida y su catálogo de habilidades propio.
+     */
     public Korby() {
         super("Korby", 100, new CatalogoKorby());
     }
