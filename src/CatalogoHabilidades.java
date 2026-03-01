1b79aefe1cce94bcb3147b12f4632c476727f82c
Christian Gutiérrez <91112471+ChristianGtz@users.noreply.github.com>
2026-03-01 16:40:27 -0600

    Update CatalogoHabilidades.java

--- src/CatalogoHabilidades.java
+++ src/CatalogoHabilidades.java
@@ -1,5 +1,8 @@
 import java.util.List;
-
+/**
+ * Interfaz que define el contrato para los catálogos de habilidades de cada personaje.
+ *
+*/
 public interface CatalogoHabilidades {
    
     List<ComportamientoHabilidad> getHabilidades();
