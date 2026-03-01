6d28714b231d911631e4e5bc8292cc63f36357dd
Christian Gutiérrez <91112471+ChristianGtz@users.noreply.github.com>
2026-03-01 16:38:14 -0600

    Update Dittuu.java

--- src/Dittuu.java
+++ src/Dittuu.java
@@ -1,5 +1,12 @@
+/**
+ * Representa al personaje Dittuu, un chinpokomon capaz de transformarse
+ * en cualquier otro chinpokomon que vea, copiando sus características.
+ */
 public class Dittuu extends Personaje {
-   
+
+    /**
+     * Construye a Dittuu con 100 puntos de vida y su catálogo de habilidades propio.
+     */
     public Dittuu() {
         super("Dittuu", 100, new CatalogoDittuu());
     }
