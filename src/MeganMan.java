ea4ee1d5aef5b3f9b4613d8b62091cbb46b8a9d4
Christian Gutiérrez <91112471+ChristianGtz@users.noreply.github.com>
2026-03-01 16:39:31 -0600

    Update MeganMan.java

--- src/MeganMan.java
+++ src/MeganMan.java
@@ -1,5 +1,7 @@
 public class MeganMan extends Personaje {
-    
+        /**
+     * Construye a MeganMan con 100 puntos de vida y su catálogo de habilidades propio.
+     */
     public MeganMan() {
         super("MeganMan", 100, new CatalogoMeganMan());
     }
